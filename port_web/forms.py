5282d961fc267f794af456e00ed090f39efe0286
MaloneMKD <132443562+MaloneMKD@users.noreply.github.com>
2024-02-17 14:08:29 +0200

    Added validation on name field

--- port_web/forms.py
+++ port_web/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, EmailField, SubmitField
-from wtforms.validators import InputRequired, Email
+from wtforms.validators import InputRequired, Email, ValidationError
 
 
 class ContactForm(FlaskForm):
@@ -8,3 +8,8 @@ class ContactForm(FlaskForm):
     email = EmailField("Email:", validators=[InputRequired(), Email()])
     message = TextAreaField("Message:", validators=([InputRequired()]))
     submit = SubmitField("Submit")
+
+    def validate_name(self, name):
+        if name.data.isnumeric():
+            raise ValidationError("Name must be made up of characters and numbers")
+
